fea2052a91a8a83608cc494e93fa412e1dee269c
Haryce <148765923+Haryce@users.noreply.github.com>
2025-02-07 10:36:30 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input;
+    cout << "введите строку: ";
+    getline(cin, input);
+    string res;
+    int sp = 0;
+    bool inWord = false;
+    for (char x : input) {
+        if (x != ' ') {
+            res += x; 
+            inWord = true; 
+        } else {
+            if (inWord) {
+                res += ' '; 
+                inWord = false; 
+            }
+            sp++; 
+        }
+    }
+    if (!res.empty() && res.back() == ' ') {
+        res.pop_back();
+    }
+    cout << "измененние в строке: " << res << endl;
+    cout << "количество удаленных пробелов: " << sp << endl;
+    return 0;
+}
